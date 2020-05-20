1afa98fe5420e062742ffd9dd338305c991645a4
qwergg <1127685207@qq.com>
2020-05-20 14:54:35 +0800

    打包切换cdn

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,7 @@ module.exports = {
     ['import', {
       'libraryName': 'ant-design-vue',
       'libraryDirectory': 'es',
-      'style': "css" // `style: true` 会加载 less 文件
+      'style': true // `style: true` 会加载 less 文件
     }]
   ]
 }
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin') // 去掉注释
-const isProduction = process.env.NODE_ENV === 'production';
+
+const isProduction = process.env.NODE_ENV === 'production';//是否是生产环境
 const plugins = [];
 plugins.push(
     new UglifyJsPlugin({
@@ -16,14 +17,50 @@ plugins.push(
         }
     })
 )
+
+const assetsCdn={
+    externals: {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        vuex: 'Vuex',
+        axios: 'axios'
+      },
+      css:[],
+      js: [
+        'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
+        'https://cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js',
+        'https://cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
+        'https://cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js'
+      ]
+}
+
+
 module.exports={
     publicPath: './',//打包引入静态资源路径
     lintOnSave: false, // 关闭eslint
-    productionSourceMap:true,//是否产生map文件 生产环境可以关掉 默认打开
-  
+    productionSourceMap:false,
+    css:{
+        loaderOptions: {
+            less: {
+                lessOptions:{
+                    javascriptEnabled: true
+                }
+                }
+            }
+          
+    },
     configureWebpack: config=>{
         if(isProduction){
             plugins
         }
+        config.externals=isProduction ? assetsCdn.externals : {}
+    },
+    chainWebpack : config=>{
+       if(isProduction){ //prod  build  配置cdn 替换vue vuex axios vur-router
+           config.plugin('html').tap(args => {
+               args[0].cdn = assetsCdn
+               return args;
+           })
+       }
     }
 }
