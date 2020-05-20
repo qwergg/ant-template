f8ec8d86d5ff5f430c3c61bf8eaab7a3b56b5b9a
qwergg <1127685207@qq.com>
2020-05-20 17:49:30 +0800

    生产d环境打包自动配置cdn

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,11 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  plugins:[ //使用按需加载ant ui
-    ['import', {
-      'libraryName': 'ant-design-vue',
-      'libraryDirectory': 'es',
-      'style': true // `style: true` 会加载 less 文件
-    }]
-  ]
+  // plugins:[ //使用按需加载ant ui
+  //   ['import', {
+  //     'libraryName': 'ant-design-vue',
+  //     'libraryDirectory': 'es',
+  //     'style': true // `style: true` 会加载 less 文件
+  //   }]
+  // ]
 }
--- vue.config.js
+++ vue.config.js
@@ -23,14 +23,18 @@ const assetsCdn={
         vue: 'Vue',
         'vue-router': 'VueRouter',
         vuex: 'Vuex',
-        axios: 'axios'
+        axios: 'axios',
+        'ant-design-vue': 'antd'
       },
-      css:[],
+      css:[
+          'https://cdn.jsdelivr.net/npm/ant-design-vue@1.6.0/dist/antd.min.css'
+      ],
       js: [
         'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
         'https://cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js',
         'https://cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
-        'https://cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js'
+        'https://cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js',
+        'https://cdn.jsdelivr.net/npm/ant-design-vue@1.6.0/dist/antd.min.js'
       ]
 }
 
@@ -54,13 +58,38 @@ module.exports={
             plugins
         }
         config.externals=isProduction ? assetsCdn.externals : {}
+        
+        // 开启分离js
+        config.optimization = {
+            runtimeChunk: 'single',
+            splitChunks: {
+                chunks: 'all',
+                maxInitialRequests: Infinity,
+                minSize: 1000 * 60,
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name(module) {
+                            // 排除node_modules 然后吧 @ 替换为空 ,考虑到服务器的兼容
+                            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                            return `npm.${packageName.replace('@', '')}`
+                        }
+                    }
+                }
+            }
+        }
+
     },
     chainWebpack : config=>{
+       // webpack 会默认给commonChunk打进chunk-vendors，所以需要对webpack的配置进行delete
+       config.optimization.delete('splitChunks')
+
        if(isProduction){ //prod  build  配置cdn 替换vue vuex axios vur-router
            config.plugin('html').tap(args => {
                args[0].cdn = assetsCdn
                return args;
            })
        }
+
     }
 }
